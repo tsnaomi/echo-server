7d68a76a4b383c4c3836681a2952a557d1628a5f
Naomi T. S. <tsnaomi@users.noreply.github.com>
2014-02-18 18:39:43 -0800

    Fix spaces

--- echo_server.py
+++ echo_server.py
@@ -6,32 +6,32 @@ import socket
 
 def data(x, y):
     data = ''
-	while True:
-		buff = x.recv(y)
-		data += buff
-		if not buff:
-			return data
+    while True:
+        buff = x.recv(y)
+        data += buff
+        if not buff:
+            return data
 
 def echo_server():
-	host = '' 
-	port = 50001 
-	backlog = 5 
-	buffer_size = 32
+    host = '' 
+    port = 50001 
+    backlog = 5 
+    buffer_size = 32
 	# ^ http://ilab.cs.byu.edu/python/socket/echoserver.html
 
-	server_socket = socket.socket(
-		socket.AF_INET,
-		socket.SOCK_STREAM,
-		socket.IPPROTO_IP)
-	server_socket.bind((host, port))
-	server_socket.listen(backlog)
+    server_socket = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_IP)
+    server_socket.bind((host, port))
+    server_socket.listen(backlog)
 
-	while True: 
-		conn, addr = server_socket.accept() 
-		conn.sendall(data(conn, buffer_size))
-		conn.close()
+    while True: 
+        conn, addr = server_socket.accept() 
+        conn.sendall(data(conn, buffer_size))
+        conn.close()
 
-	server_socket.close()
+    server_socket.close()
 
 if __name__ == '__main__':
-	echo_server()
+    echo_server()
